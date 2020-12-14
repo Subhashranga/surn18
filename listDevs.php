d124ed20f86605d6ccce54baf84f3349e9450b52
Subhashranga <bbb.ranga@gmail.com>
2020-12-14 22:08:16 +0100

    final

--- listDevs.php
+++ listDevs.php
@@ -1,22 +1,13 @@
 <?php
 include_once('config.php');
 
-$result = $db->query('SELECT * FROM info');
+$output = $database->query('SELECT * FROM switches');
 
-
-while ($device_info = $result->fetchArray()) {	
-	echo "\n";
-	echo $device_info[0]. "|" .$device_info[1]. "|" .$device_info[2]. "|" .$device_info[3]. "|" .$device_info[4]. "|" .$device_info[5]. "|" .$device_info[6]."|";
-
-    if (empty($device_info[0]) && empty($device_info[1]) && empty($device_info[2]) && empty($device_info[3]) && empty($device_info[4]) && empty($device_info[5]) &&  empty($device_info[6])){
-    	echo "empty";
-
-    }
-
+while ($device = $output->fetchArray()) {
+    echo $device[0]. "|" .$device[1]. "|" .$device[2]. "|" .$device[3]. "|" .$device[4]. "|" .$device[5]. "|" .$device[6]. "\n";
 }
 
-
-$db->close();
+$database->close();
 
 ?>
 
